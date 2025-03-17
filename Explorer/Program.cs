52dc3f720b8a6ff55a29c7a7ccd0b0ae6ae755d9
Mark Overton <37475995+moverton99@users.noreply.github.com>
2025-03-17 10:52:32 -0400

    v2

--- Explorer/Program.cs
+++ Explorer/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Add static files middleware FIRST
+app.UseStaticFiles();
+
+// Then define endpoints
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
